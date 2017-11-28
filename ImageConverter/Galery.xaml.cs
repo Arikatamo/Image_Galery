572ed7e541c1d384c1ef0a76c0fff8c7b7140f01
Arikatamo <arikatamo25@gmail.com>
2017-11-28 16:36:54 +0200

    ll

---
+++ ImageConverter/Galery.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+namespace ImageConverter
+{
+    /// <summary>
+    /// Interaction logic for Galery.xaml
+    /// </summary>
+    public partial class Galery : MetroWindow
+    {
+        public Galery()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
