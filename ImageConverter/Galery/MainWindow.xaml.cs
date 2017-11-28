017dcf2969999fc5dbd6e39fd209de2da5dba333
Arikatamo <arikatamo25@gmail.com>
2017-11-28 19:21:25 +0200

    Карнтиник

--- ImageConverter/Galery/MainWindow.xaml.cs
+++ ImageConverter/Galery/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using System.IO;
 namespace Galery
 {
     /// <summary>
@@ -23,6 +24,21 @@ namespace Galery
         public MainWindow()
         {
             InitializeComponent();
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Arikatamo\Documents\Case\Нова папка\Hearts");
+            foreach (var item in dir.GetFiles())
+            {
+  
+                Image images = new Image();
+                images.Source = new BitmapImage(new Uri(@"C:\Users\Arikatamo\Documents\Case\Нова папка\Hearts\" + item));
+                images.RenderSize = new Size(100, 100);
+                images.Margin = new Thickness(10, 10, 10, 10);
+                Image_Galery.Children.Add(images);
+   
+               }
+        }
+
+        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
 
         }
     }
